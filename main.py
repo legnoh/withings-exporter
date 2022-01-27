76a0ef4fb70ea6bd5735edfbfa8462454cb991c0
Ryoma Kai <legknmj@icloud.com>
2022-01-27 09:46:58 +0900

    add null logics

--- main.py
+++ main.py
@@ -40,49 +40,57 @@ with open('config/metrics.yml', 'r') as stream:
 logging.info("gathering measurement data...")
 measuregrps = wi.get_latest_meas_datas(credentials.access_token)
 meas_metrics = {}
-for measuregrp in measuregrps:
-    for measure in measuregrp['measures']:
-        labels = [
-            measuregrp['category'],
-            measuregrp['hash_deviceid'],
-        ]
-        if measure['type'] not in meas_metrics:
-            meas_metrics[measure['type']] = wi.create_metric_instance(config['meas']['metrics'][measure['type']], registry, 'withings_meas_')
-        wi.set_metrics(meas_metrics[measure['type']], labels, measure['value'] * (10 ** measure['unit']))
+if measuregrps == None:
+    logging.info("no measurement data... skipped")
+else:
+    for measuregrp in measuregrps:
+        for measure in measuregrp['measures']:
+            labels = [
+                measuregrp['category'],
+                measuregrp['hash_deviceid'],
+            ]
+            if measure['type'] not in meas_metrics:
+                meas_metrics[measure['type']] = wi.create_metric_instance(config['meas']['metrics'][measure['type']], registry, 'withings_meas_')
+            wi.set_metrics(meas_metrics[measure['type']], labels, measure['value'] * (10 ** measure['unit']))
 
 # get activities
 logging.info("gathering activity data...")
 activities = wi.get_latest_activity_datas(credentials.access_token, config['activity'])
-activity_metrics = {}
-for activity in activities:
-    labels = [
-        activity['hash_deviceid'],
-        activity['brand'],
-        activity['is_tracker']
-    ]
+if activities == None:
+    logging.info("no activity data... skipped")
+else:
+    activity_metrics = {}
+    for activity in activities:
+        labels = [
+            activity['hash_deviceid'],
+            activity['brand'],
+            activity['is_tracker']
+        ]
 
-    for k, v in activity.items():
-        if k in config['activity']['labels']['key'] or k in config['activity']['labels']['exclude']:
-            continue
-        if k not in activity_metrics:
-            activity_metrics[k] = wi.create_metric_instance(config['activity']['metrics'][k], registry, 'withings_activity_')
-        wi.set_metrics(activity_metrics[k], labels, v)
+        for k, v in activity.items():
+            if k in config['activity']['labels']['key'] or k in config['activity']['labels']['exclude']:
+                continue
+            if k not in activity_metrics:
+                activity_metrics[k] = wi.create_metric_instance(config['activity']['metrics'][k], registry, 'withings_activity_')
+            wi.set_metrics(activity_metrics[k], labels, v)
 
 # get sleep series
 logging.info("gathering sleep data...")
 sleepseries = wi.get_latest_sleep_datas(credentials.access_token, config['sleep'])
-sleep_metrics = {}
-for sleep_data in sleepseries:
-    labels = [
-        sleep_data['model'],
-        sleep_data['model_id'],
-        sleep_data['hash_deviceid'],
-    ]
-    for k, v in sleep_data['data'].items():
-        if k not in sleep_metrics:
-            sleep_metrics[k] = wi.create_metric_instance(config['sleep']['metrics'][k], registry, 'withings_sleep_')
-        wi.set_metrics(sleep_metrics[k], labels, v)
-
+if sleepseries == None:
+    logging.info("no sleepseries data... skipped")
+else:
+    sleep_metrics = {}
+    for sleep_data in sleepseries:
+        labels = [
+            sleep_data['model'],
+            sleep_data['model_id'],
+            sleep_data['hash_deviceid'],
+        ]
+        for k, v in sleep_data['data'].items():
+            if k not in sleep_metrics:
+                sleep_metrics[k] = wi.create_metric_instance(config['sleep']['metrics'][k], registry, 'withings_sleep_')
+            wi.set_metrics(sleep_metrics[k], labels, v)
 
 write_to_textfile('./container/public/withings.prom', registry)
 logging.info("gathering health data successfull!")
